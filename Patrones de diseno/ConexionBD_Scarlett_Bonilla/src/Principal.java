21f09412dd6830240fa9e410b540479c52d4ba05
scarlett9895 <scarlett.bonilla@estudiantes.utec.edu.uy>
2021-08-19 19:58:46 -0300

    este es el proyecto de la semana 3

--- Patrones de diseno/ConexionBD_Scarlett_Bonilla/src/Principal.java
+++ Patrones de diseno/ConexionBD_Scarlett_Bonilla/src/Principal.java
@@ -4,60 +4,32 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.LinkedList;
 
 import javax.swing.JOptionPane;
 
 public class Principal {
 
+	static LinkedList<String> personas = null;
 	public static void main(String[] args) {
 		Connection connection = DatabaseManager.getConnection();
-		//String apellido =  JOptionPane.showInputDialog("Ingrese el apellido a buscar: ");
-		String idPersonaST = JOptionPane.showInputDialog("Id: ");
-		Integer idPersona; // Integer es el tipo wrapper del tipo nativo int
-		idPersona = Integer.parseInt(idPersonaST);
-
-		String documento = JOptionPane.showInputDialog("Documento: ");
-		String apellido1 = JOptionPane.showInputDialog("Apellido1: ");
-		String apellido2 = JOptionPane.showInputDialog("Apellido2: ");
-		String nombre1 = JOptionPane.showInputDialog("Nombre1: ");
-		String nombre2 = JOptionPane.showInputDialog("Nombre2: ");
-
-		String INSERT_PERSONA="INSERT INTO PERSONA (ID_PERSONA,DOCUMENTO,APELLIDO1,APELLIDO2,NOMBRE1,NOMBRE2)"
-				+ " values (?,?,?,?,?,?)";
-
-
-		try {//Intentamos ejecutar el requerimiento sobre base de datos
-			PreparedStatement statement=connection.prepareStatement(INSERT_PERSONA);
-			statement.setInt(1, idPersona);
-			statement.setString(2, documento);
-			statement.setString(3, apellido1);
-			statement.setString(4, apellido2);
-			statement.setString(5, nombre1);
-			statement.setString(6, nombre2);
-			//Ejecutamos la sentencia
-			int filasIngresadas = statement.executeUpdate();
-
-			//Chequeamos que se haya agregado la nueva persona
-			String consulta = "SELECT * FROM persona";
-			System.out.println(consulta);
-
-			Statement sentencia = connection.createStatement();
-			ResultSet personaRS = sentencia.executeQuery(consulta);
-
-			boolean hayCoincidencias = false;
-			while (personaRS.next()) {
-				if (!hayCoincidencias) hayCoincidencias = true;
-				System.out.println(personaRS.getString("NOMBRE1") + " " + personaRS.getString("NOMBRE2"));
-
-			} 
-			if (!hayCoincidencias) {
-
-				JOptionPane.showMessageDialog(null, "No hay datos para mostrar");
-			}
-
-		} catch (Exception e) {
-			System.out.println(e);
+		System.out.println();
+		System.out.println("********* DAO PERSONA **********");
+		System.out.println();
+		int cuenta = DAOPersona.cuentaPersonas();
+		
+		if(cuenta>0) {
+			System.out.println("HAY " + cuenta + " REGISTROS EN LA TABLA PERSONA");
+		} else {
+			System.out.println("NO HAY REGISTROS EN LA TABLA PERSONA");
 		}
+		
+		
+		personas = DAOPersona.findAll();
+		
+		Ventana v = new Ventana();
+		v.getFrame().setVisible(true);
+		
 		DatabaseManager.cerrarConexion(connection);
 	}
 
